1d58f22fc6e421eb1e40879ae147282e93f4e2e7
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-03 21:12:25 +0530

    patt

--- 02-patterns/pattern-05.js
+++ 02-patterns/pattern-05.js
@@ -17,11 +17,26 @@
 // 1234
 
 
-for ( let row=0; row<4; row++){
-    let pattern = "";
-     for(let col=1; col<=row+1; col++){
-        pattern += col;
-     }
-     console.log(pattern)
-}
+// for ( let row=0; row<4; row++){
+//     let pattern = "";
+//      for(let col=1; col<=row+1; col++){
+//         pattern += col;
+//      }
+//      console.log(pattern)
+// }
 
+
+
+// 1
+// 22
+// 333
+// 4444
+
+
+for(let row =1; row<=4; row++){
+    let pattern = "";
+      for (let col =1; col<=row; col++){
+        pattern  += row;
+      }
+      console.log(pattern)
+}

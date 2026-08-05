df48259e944b858864838c4e19850caaf285a2a5
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-05 09:47:42 +0530

    more patterns

--- 02-patterns/pattern-04.js
+++ 02-patterns/pattern-04.js
@@ -4,11 +4,29 @@
 // 1234
 
 
-for(let row =0; row<4; row++){
-    let  pattern = "";
-      for(let col =1; col<=row+1;col++){
-        pattern += col
-      }
-      console.log(pattern)
+// for(let row =0; row<4; row++){
+//     let  pattern = "";
+//       for(let col =1; col<=row+1;col++){
+//         pattern += col
+//       }
+//       console.log(pattern)
+// }
+
+
+
+// 5
+// 44
+// 333
+// 2222
+// 11111
+
+
+for(let i= 0;i<5;i++){
+  let pattern = "";
+  for(let j= 0; j<i+1;j++){
+    pattern += 5-i
+  }
+  console.log(pattern)
 }
+
 

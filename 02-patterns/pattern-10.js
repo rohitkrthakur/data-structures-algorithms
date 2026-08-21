4aaa7c4668d45112ac35b6f790833b6d2961f692
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-21 22:14:01 +0530

    to to bottom

--- 02-patterns/pattern-10.js
+++ 02-patterns/pattern-10.js
@@ -1,8 +1,19 @@
+// for (let i = 0; i < 5; i++) {
+//     let pattern = "";
+
+//     for (let j = 1; j <= 5 - i; j++) {
+//         pattern += j;
+//     }
+
+//     console.log(pattern);
+// }
+
+
 for (let i = 0; i < 5; i++) {
     let pattern = "";
 
-    for (let j = 1; j <= 5 - i; j++) {
-        pattern += j;
+    for (let j = 0; j < i + 1; j++) {
+        pattern += i + 1;
     }
 
     console.log(pattern);
